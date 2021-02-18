5031ecb7775876c0479e80e7fc5262daa4e9c9d2
James Grieve <pocojames@hotmail.com>
2021-02-18 14:47:17 -0800

    finish Q2

--- chapter1.rb
+++ chapter1.rb
@@ -4,3 +4,20 @@
 def unique?(str)
     str.split("").uniq.length == str.split("").length 
 end
+
+puts unique?('abcd') # => true
+puts unique?('abccd') # => false
+puts unique?('bhjjb') # => false
+puts unique?('mdjq') # => true
+# Question 2
+# Check Permutation: Given two strings,write a method to decide if one is a permutation of the
+# other
+
+def is_permutation?(s1, s2)
+    return false if s1.length != s2.length
+    s1.split("").sort == s2.split("").sort
+end
+
+puts is_permutation?('aba', 'aab') # => true
+puts is_permutation?('aba', 'aaba') # => false
+puts is_permutation?('aba', 'aa') # => false
